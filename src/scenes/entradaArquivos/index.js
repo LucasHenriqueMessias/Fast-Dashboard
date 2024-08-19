381752b30efc54a7d88473fe1235e824d6bba649
lucashenriquemessias <lucashenriquemessiasgoncalves@gmail.com>
2024-08-19 10:58:46 -0300

    excel to json extractor

--- src/scenes/entradaArquivos/index.js
+++ src/scenes/entradaArquivos/index.js
@@ -8,43 +8,41 @@ import * as XLSX from 'xlsx';
 
 
 const EntradaArquivos = () => {
-    const [data, setData] = useState(null);
+  const [data, setData] = useState(null);
 
 
-    const handleFileUpload = (e) => {
+  const handleFileUpload = (e) => {
     const file = e.target.files[0];
     const reader = new FileReader();
-      
+
     reader.onload = (event) => {
-        const workbook = XLSX.read(event.target.result, { type: 'array' });
-        const sheetName = workbook.SheetNames[3];
-        const sheet = workbook.Sheets[sheetName];
-        
+      const workbook = XLSX.read(event.target.result, { type: 'array' });
+      const sheetName = workbook.SheetNames[3];
+      const sheet = workbook.Sheets[sheetName];
 
-        const minRow = 3;
 
-        let data = XLSX.utils.sheet_to_json(sheet);
-        console.log(data);
-        setData(data)
+      const minRow = 3;
+
+      let data = XLSX.utils.sheet_to_json(sheet);
+      console.log(data);
+      setData(data)
     };
 
     reader.readAsArrayBuffer(file);
-  
-    };
-  
-    return (
-      <div>
-        <Input type="file" onChange={handleFileUpload} />
-        {data && (
-          <div>
-            <h2>Imported Data:</h2>
-            <pre>{JSON.stringify(data, null, 2)}</pre>
-          </div>
-        )}
-      </div>
-    );
 
+  };
 
+  return (
+    <div>
+      <Input type="file" onChange={handleFileUpload} />
+      {data && (
+        <div>
+          <h2>Imported Data:</h2>
+          <pre>{JSON.stringify(data, null, 2)}</pre>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default EntradaArquivos;
